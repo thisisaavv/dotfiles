ec63cf8edbeb1b2516aa90b7bb57be5f3094b900
Alexis Vallecillo <thisisaavv@gmail.com>
2023-09-22 02:52:49 -0600

    feat: keyboard remapping

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -4,6 +4,7 @@ lsp.preset("recommended")
 
 lsp.ensure_installed({
 	'tsserver',
+    'eslint',
 	'rust_analyzer',
 })
 
@@ -16,8 +17,8 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+	['<C-K>'] = cmp.mapping.confirm({ select = true }),
+	['<C-Space>'] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
